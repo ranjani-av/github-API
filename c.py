2d4c0e80cb3263d994d48baedbfb5971172206e9
ranjani-av <52025392+ranjani-av@users.noreply.github.com>
2019-10-09 16:05:09 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,28 +1,48 @@
-# entering name, msg, author, time of the latest commit into csv file
+#entering the all the commit details in the repository(multiple pages)
+
+import re
 import json
 import requests
 
-commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits')	
-d=commit.json()
-#d['values'][0]['links']
-x1=requests.get(d['values'][0]['links']['self']['href'])
-d1=x1.json()
-
-x2= requests.get(d['values'][0]['links']['diff']['href'])
-d2=x2.text
-#print(d2)
-import re
-m=re.match(r"^diff --git a/(.*) b",d2)
-#m.groups(1)[0]
+name=[]
+msg=[]
+author=[]
+date=[]
 
 import csv
-commit_name=m.groups(1)[0]
-msg=d1['message']
-commit_author=d1['author']['raw']
-commit_date=d1['date']
-with open('commits1.csv', 'w', newline= '') as f:
-	f_names=['Commit name', 'commit mesage', 'commit author', 'Commit date&time']
-	thewriter = csv.DictWriter(f, fieldnames=f_names)
-	thewriter.writeheader()
-	thewriter.writerow({'Commit name':commit_name, 'commit mesage':msg, 'commit author':commit_author, 'Commit date&time':commit_date})
-	f.close()
+with open('commits4.csv', 'w', newline= '') as f:
+    f_names=['Commit Name', 'Commit Mesage', 'Commit Author', 'Commit Date&Time']
+    thewriter = csv.DictWriter(f, fieldnames=f_names)
+    thewriter.writeheader()
+
+    for j in range(1,4):
+        #print(j)
+        commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits/?page=%d'%j)
+        #commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits')
+        d=commit.json()
+        #print(d['values'])
+
+        for i in range(len(d['values'])):
+            x1=requests.get(d['values'][i]['links']['self']['href'])
+            d1=x1.json()
+            x2= requests.get(d['values'][i]['links']['diff']['href'])
+            d2=x2.text
+            m=re.match(r"^diff --git a/(.*) b",d2)
+            
+            cname=m.groups(1)[0]
+            cmsg=d1['message']
+            cauthor=d1['author']['raw']
+            cdate=d1['date']
+            
+            name.append(cname)
+            msg.append(cmsg)
+            author.append(cauthor)
+            date.append(cdate)
+            
+            #print(d1['message'])
+            
+            thewriter.writerow({'Commit Name':cname, 'Commit Mesage':cmsg, 'Commit Author':cauthor, 'Commit Date&Time':cdate})
+
+    f.close()
+
+print(len(name),len(msg),len(author),len(date))
