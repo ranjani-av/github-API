7170bfb2260c86a798007c171918770bcef22ab8
ranjani-av <52025392+ranjani-av@users.noreply.github.com>
2019-10-09 16:04:23 +0530

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,25 +1,31 @@
-#entering the commit msgs of the first page
-import re
+#entering the all the commit msgs in the repository(multiple pages)
+
+#import re
 import json
 import requests
-commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits')
-d=commit.json()
 
 msg=[]
 
 import csv
-with open('commits2.csv', 'w', newline= '') as f:
+with open('commits3.csv', 'w', newline= '') as f:
     f_names=['Commit Mesage']
     thewriter = csv.DictWriter(f, fieldnames=f_names)
     thewriter.writeheader()
 
-    for i in range(len(d['values'])):
-        x1=requests.get(d['values'][i]['links']['self']['href'])
-        d1=x1.json()
-        cmsg=d1['message']
-        msg.append(cmsg)
-        #print(d1['message'])
-        thewriter.writerow({'Commit Mesage':cmsg})
+    for j in range(1,4):
+        print(j)
+        commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits/?page=%d'%j)
+        #commit=requests.get('https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabph/ph48/commits')
+        d=commit.json()
+        print(d['values'])
+
+        for i in range(len(d['values'])):
+            x1=requests.get(d['values'][i]['links']['self']['href'])
+            d1=x1.json()
+            cmsg=d1['message']
+            msg.append(cmsg)
+            #print(d1['message'])
+            thewriter.writerow({'Commit Mesage':cmsg})
 
     f.close()
 
